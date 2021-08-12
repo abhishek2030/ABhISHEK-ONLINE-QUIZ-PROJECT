76e489361ed2ba039fe5bfc0d2490361647db45f
abhishekmunigety <61018242+abhishek2030@users.noreply.github.com>
2021-08-12 22:30:53 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,120 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "java.util.Collections is a",
+    answer: "A. Class",
+    options: [
+      "A. Class",
+      "B. Interface",
+      "c. Object",
+      "D. None of the above"
+    ]
+  },
+    {
+    numb: 2,
+    question: "LinkedList implements?",
+    answer: "C. Both A and B",
+    options: [
+      "A. List",
+      "B. Deque",
+      "C. Both A and B",
+      "D. None of the above"
+    ]
+  },
+
+  {
+    numb: 3,
+    question: "Which of those doesn’t have an index based structure?",
+    answer: "B. set",
+    options: [
+      "A. List",
+      "B. set",
+      "C. Map",
+      "D. None of the above."
+      
+    ]
+  },
+    {
+    numb: 4,
+    question: "In which case, a program is expected to recover?",
+    answer: "B. If an exception occurs.",
+    options: [
+      "A. If an error accours",
+      "B. If an exception occurs.",
+      "C. Both of the above.",
+      "D. None of the above."
+    ]
+  },
+    {
+    numb: 5,
+    question: "What will happen if static modifier is removed from the signature of the main method?",
+    answer: "D. RunTime Error: NoSuchMethodError.",
+    options: [
+      "A. Program compiles and gives an Output.",
+      "B. Program will compile and run without any output.",
+      "C. Compilation error.",
+      "D. RunTime Error: NoSuchMethodError."
+    ]
+  },
+
+    {
+    numb: 6,
+    question: "What does XML stand for?",
+    answer: "A. eXtensible Markup Language",
+    options: [
+    "A. eXtensible Markup Language",
+    "B. eXecutable Multiple Language",
+    "C. eXTra Multi-Program Language",
+    "D. eXamine Multiple Language"
+    ]
+  },
+  
+   {
+    numb: 7,
+    question: "What is the size of int variable?",
+    answer: "C. 32 bit",
+    options: [
+    "A. 8 bit",
+    "B. 64 bit",
+    "C. 32 bit",
+    "D. 16 bit"
+   ]
+   },
+
+   {
+    numb: 8,
+    question: "When static binding occurs?",
+    answer: "B. Static binding occurs during Compile time.",
+    options: [
+    "A. Static binding occurs during load time.",
+    "B. Static binding occurs during Compile time.",
+    "C. Static binding occurs during run time",
+    "D. None of the above."
+   ]
+   },
+
+   {
+    numb: 9,
+    question: "What happens when thread's sleep() method is called?",
+    answer: "A. Thread returns to the waiting state.",
+    options: [
+    "A. Thread returns to the waiting state.",
+    "B. Thread returns to the steady state.",
+    "C. Thread starts running.",
+    "D. None of the above."
+   ]
+   },
+
+   {
+    numb: 10,
+    question: "Which of the following is false about String?",
+    answer: "C. String is a primary data type.",
+    options: [
+    "A. String is immutable",
+    "B. String can be created using new operator.",
+    "C. String is a primary data type.",
+    "D. None of the above."
+   ]
+   },
+];
